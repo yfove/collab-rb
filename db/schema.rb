438a7ca9e0765199bdb5bea54e39278e60a03a88
Curtis C <curtis.chat.hk@gmail.com>
2018-09-24 17:38:18 -0400

    add image to projects model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_21_042948) do
+ActiveRecord::Schema.define(version: 2018_09_24_212236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2018_09_21_042948) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
